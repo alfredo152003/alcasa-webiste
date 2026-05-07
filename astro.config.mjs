4ff771f3cc1b68df3fa585d86da9e508c35744ad
Alfredo Mirón <josealfredomiron@gmail.com>
2026-05-07 12:35:48 -0600

    Configurar deploy en github pages y cambios estéticos + nueva página

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,27 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        host: true,
-        port: 4321,
-    },
-    vite: {
-      server: {
-          watch: {
-              usePolling: true,   // ← solución para Docker en Windows
-              interval: 500,      // revisa cambios cada 500ms
-          }
-      },
+  site: 'https://Alfredo152003.github.io',
+  base: '/Alcasa-Webiste',
+  server: {
+      host: true,
+      port: 4321,
+  },
 
-      plugins: [tailwindcss()]
-    }
+  vite: {
+    server: {
+        watch: {
+            usePolling: true,   // ← solución para Docker en Windows
+            interval: 500,      // revisa cambios cada 500ms
+        }
+    },
+
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap()]
 });
